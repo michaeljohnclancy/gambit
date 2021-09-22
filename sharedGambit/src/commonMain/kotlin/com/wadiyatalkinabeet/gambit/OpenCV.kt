f89f256e9d254b653b5619d1e2b0c5e58c4afb1f
Michael Clancy <mjclancy@protonmail.ch>
2021-09-22 22:49:48 +0100

    OpenCV functions exposed through expect/actual into commonMain

--- sharedGambit/src/commonMain/kotlin/com/wadiyatalkinabeet/gambit/OpenCV.kt
+++ sharedGambit/src/commonMain/kotlin/com/wadiyatalkinabeet/gambit/OpenCV.kt
@@ -8,10 +8,24 @@ package com.wadiyatalkinabeet.gambit
 //expect fun randomUUID(): String
 
 expect class Mat
+expect class MatOfPoint2f
+expect class Size
 
 expect fun loadChessboardExampleImage(): Mat
 
-expect fun canny(matIn: Mat, matOut: Mat, lowerThreshold: Double, upperThreshold: Double, apertureSize: Int)
+expect fun cvtColor(src: Mat, dst: Mat, colorOut: Int)
+
+expect fun resize(src: Mat, dst: Mat, dsize: Size)
+
+expect fun canny(src: Mat, dst: Mat, lowerThreshold: Double, upperThreshold: Double, apertureSize: Int)
+
+expect fun houghLines(src: Mat, lines: Mat, rho: Double, theta: Double, threshold: Int, srn: Double)
+
+expect fun findHomography(srcPoints: MatOfPoint2f, dstPoints: MatOfPoint2f): Mat
+
+expect fun warpPerspective(src: Mat, dst: Mat, transformationMatrix: Mat, dsize: Size)
+
+expect fun sobel(src: Mat, dst: Mat, ddepth: Int, dx: Int, dy: Int)
 
 fun processImage(matIn: Mat, matOut: Mat, lowerThreshold: Double, upperThreshold: Double, apertureSize: Int): Mat {
     canny(matIn, matOut, lowerThreshold, upperThreshold, apertureSize)
