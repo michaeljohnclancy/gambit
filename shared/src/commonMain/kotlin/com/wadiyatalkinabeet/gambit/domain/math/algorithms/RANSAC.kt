3dccdc4b654e7566c59c737e81b4c74626e9f763
michaeljohnclancy <mjclancy@protonmail.ch>
2021-11-11 10:02:23 +0000

    Fixed warpPoints, dividing by the Z Coordinate when converting from homogenous to cartesian coordinates

--- shared/src/commonMain/kotlin/com/wadiyatalkinabeet/gambit/domain/math/algorithms/RANSAC.kt
+++ shared/src/commonMain/kotlin/com/wadiyatalkinabeet/gambit/domain/math/algorithms/RANSAC.kt
@@ -10,19 +10,18 @@ private fun warpPoint(
     y: Float,
     z: Float,
     transformationMatrix: Mat
-): Triple<Float, Float, Float> {
+): Point {
 
     // TODO: Matrix multiplication could be handled by OpenCV, no need for manual
     val warpedX = (x * transformationMatrix[0, 0][0]) + (y * transformationMatrix[0, 1][0]) + (z * transformationMatrix[0, 2][0])
     val warpedY = (x * transformationMatrix[1, 0][0]) + (y * transformationMatrix[1, 1][0]) + (z * transformationMatrix[1, 2][0])
     val warpedZ = (x * transformationMatrix[2, 0][0]) + (y * transformationMatrix[2, 1][0]) + (z * transformationMatrix[2, 2][0])
 
-    return Triple(warpedX, warpedY, warpedZ)
+    return Point(warpedX / warpedZ, warpedY / warpedZ)
 }
 
 fun warpPoint(cornerPoint: Point, transformationMat: Mat): Point{
-    val (warpedX, warpedY, _) = warpPoint(cornerPoint.x, cornerPoint.y, 1.0f, transformationMat)
-    return Point(warpedX, warpedY)
+    return warpPoint(cornerPoint.x, cornerPoint.y, 1.0f, transformationMat)
 }
 
 fun warpPoints(intersectionPoints: Array<Array<Point?>>, transformationMat: Mat): Array<Array<Point?>>{
@@ -34,8 +33,7 @@ fun warpPoints(intersectionPoints: Array<Array<Point?>>, transformationMat: Mat)
     for (i in intersectionPoints.indices){
         for (j in intersectionPoints[i].indices){
             intersectionPoints[i][j]?.let {
-                val (warpedX, warpedY, _) = warpPoint(it.x, it.y, 1f, transformationMat)
-                warpedPoints[i][j] = Point(warpedX, warpedY)
+                warpedPoints[i][j] = warpPoint(it.x, it.y, 1f, transformationMat)
             }
         }
     }
